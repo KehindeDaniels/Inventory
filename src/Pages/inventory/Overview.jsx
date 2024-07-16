c6624c3ec1b144832e626eac3d8163ba66fe14aa
kehinde_Daniels <kehindedaniels501@gmail.com>
2024-07-16 15:06:52 +0100

    responsive table

--- src/Pages/inventory/Overview.jsx
+++ src/Pages/inventory/Overview.jsx
@@ -6,10 +6,16 @@ import StockTable from "../../Components/StockTable ";
 const Overview = () => {
   return (
     <div className="flex-1 bg-white p-4 sm:p-8">
-      <div className="cards">
-        <OverviewCard />
-        <Table />
-        <StockTable />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="col-span-1 md:col-span-2 lg:col-span-3">
+          <OverviewCard />
+        </div>
+        <div className="col-span-1 md:col-span-2 lg:col-span-3 overflow-x-auto">
+          <Table />
+        </div>
+        <div className="col-span-1 md:col-span-2 lg:col-span-3 overflow-x-auto">
+          <StockTable />
+        </div>
       </div>
     </div>
   );
